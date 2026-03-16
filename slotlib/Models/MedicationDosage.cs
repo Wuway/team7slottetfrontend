c21cd0e20a1e17ab4ce9ab65fdc8fc9abecac8dc
Wuway <contactdieter@gmail.com>
2026-03-16 11:24:44 +0100

    tilføjet MedicationDosage model

---
+++ slotlib/Models/MedicationDosage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace slotlib.Models
+{
+    public class MedicationDosage
+    {
+        private int _id;
+        public int Id { get; set; }
+
+        private string _dosage;
+        public string Dosage { get; set; }
+    }
+}
