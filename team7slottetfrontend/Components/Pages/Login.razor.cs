31da960dcf2fa260c0405879482a6db318fd54e2
LauraPowerHansen <laha71386@edu.ucl.dk>
2026-04-06 08:03:31 +0200

    Login html

---
+++ team7slottetfrontend/Components/Pages/Login.razor.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace team7slottetfrontend.Components.Pages
+{
+    public partial class Login : ComponentBase
+    {
+        protected string Username { get; set; } = "";
+        protected string Password { get; set; } = "";
+        protected string ErrorMessage { get; set; } = "";
+
+        protected void LoginUser()
+        {
+            if (string.IsNullOrWhiteSpace(Username) || string.IsNullOrWhiteSpace(Password))
+            {
+                ErrorMessage = "Udfyld både brugernavn og kodeord.";
+                return;
+            }
+
+            ErrorMessage = "";
+        }
+    }
+}
